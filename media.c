de8b0de4205e3fcec05a6a342f7cbd636a56501a
Fenrir Lin <fenrir.lin@rock-chips.com>
2020-02-13 09:57:01 +0800

    Modify audio and video initial value to small hump
    In addition, iID is changed to id.
    
    Change-Id: Id0cba2d37e3d723ae2fc297f344a35944f4e89a6
    Signed-off-by: Fenrir Lin <fenrir.lin@rock-chips.com>

--- media.c
+++ media.c
@@ -33,37 +33,37 @@ static int dbus_manager_init(DBusConnection *dbus_conn)
 
 void media_init(DBusConnection *dbus_conn)
 {
-    char *col_para = "iID INTEGER PRIMARY KEY," \
-                     "sStreamType TEXT DEFAULT 'MainStream'," \
-                     "sVideoType TEXT DEFAULT 'CompositeStream'," \
-                     "sResolution TEXT DEFAULT '1920*1080P'," \
+    char *col_para = "id INTEGER PRIMARY KEY," \
+                     "sStreamType TEXT DEFAULT 'mainStream'," \
+                     "sVideoType TEXT DEFAULT 'compositeStream'," \
+                     "sResolution TEXT DEFAULT '1920*1080'," \
                      "sRCMode TEXT DEFAULT 'CBR'," \
-                     "sImageQuality TEXT DEFAULT 'Medium'," \
+                     "sImageQuality TEXT DEFAULT 'medium'," \
                      "sFrameRate TEXT DEFAULT '30'," \
                      "iMaxRate INT DEFAULT 4096," \
                      "sOutputDataType TEXT DEFAULT 'H.264'," \
-                     "sSmart TEXT DEFAULT 'Close'," \
-                     "sRCQuality TEXT DEFAULT 'High'," \
+                     "sSmart TEXT DEFAULT 'close'," \
+                     "sRCQuality TEXT DEFAULT 'high'," \
                      "iGOP INT DEFAULT 30," \
-                     "sSVC TEXT DEFAULT 'Close'," \
+                     "sSVC TEXT DEFAULT 'close'," \
                      "iStreamSmooth INT DEFAULT 50";
 
     rkdb_creat(TABLE_VIDEO, col_para);
     /* TODO: Three tables need different initial values */
-    rkdb_insert(TABLE_VIDEO, "iID, sStreamType", "0, 'MainStream'");
-    rkdb_insert(TABLE_VIDEO, "iID, sStreamType", "1, 'SubStream'");
-    rkdb_insert(TABLE_VIDEO, "iID, sStreamType", "2, 'ThirdStream'");
+    rkdb_insert(TABLE_VIDEO, "id, sStreamType", "0, 'mainStream'");
+    rkdb_insert(TABLE_VIDEO, "id, sStreamType", "1, 'subStream'");
+    rkdb_insert(TABLE_VIDEO, "id, sStreamType", "2, 'thirdStream'");
 
-    col_para = "iID INTEGER PRIMARY KEY," \
+    col_para = "id INTEGER PRIMARY KEY," \
                "sEncodeType TEXT DEFAULT 'AAC'," \
                "iSampleRate INT DEFAULT 16000," \
                "iBitRate INT DEFAULT 32," \
-               "sInput TEXT DEFAULT 'MicIn'," \
+               "sInput TEXT DEFAULT 'micIn'," \
                "iVolume INT DEFAULT 50," \
-               "sANS TEXT DEFAULT 'Close'";
+               "sANS TEXT DEFAULT 'close'";
 
     rkdb_creat(TABLE_AUDIO, col_para);
-    rkdb_insert(TABLE_AUDIO, "iID", "0");
+    rkdb_insert(TABLE_AUDIO, "id", "0");
 
     dbus_manager_init(dbus_conn);
 }
