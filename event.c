09299387a91cecfab69e770703df7a18704376b0
Fenrir Lin <fenrir.lin@rock-chips.com>
2020-04-14 10:07:48 +0800

    dbserver: Add regional invasion table
    Signed-off-by: Fenrir Lin <fenrir.lin@rock-chips.com>
    Change-Id: I83f73665de30404cf3ed11432cfeefdf11bc75f9

--- event.c
+++ event.c
@@ -21,6 +21,7 @@
 #define TABLE_EVENT_TRIGGERS            "EventTriggers"
 #define TABLE_EVENT_SCHEDULES           "EventSchedules"
 #define TABLE_EVENT_MOTION_DETECTION    "EventMotionDetection"
+#define TABLE_EVENT_REGIONAL_INVASION   "EventRegionalInvasion"
 #define TABLE_FACE_CONFIG               "FaceConfig"
 #define TABLE_FACE_LIST                 "FaceList"
 #define TABLE_FACE_SNAPSHOT_RECORD      "FaceSnapshotRecord"
@@ -51,6 +52,7 @@ void event_init(DBusConnection *dbus_conn)
     g_free(rkdb_drop(TABLE_EVENT_TRIGGERS));
     g_free(rkdb_drop(TABLE_EVENT_SCHEDULES));
     g_free(rkdb_drop(TABLE_EVENT_MOTION_DETECTION));
+    g_free(rkdb_drop(TABLE_EVENT_REGIONAL_INVASION));
     g_free(rkdb_drop(TABLE_FACE_CONFIG));
     g_free(rkdb_drop(TABLE_FACE_LIST));
     g_free(rkdb_drop(TABLE_FACE_SNAPSHOT_RECORD));
@@ -103,6 +105,18 @@ void event_init(DBusConnection *dbus_conn)
                "sGridMap TEXT DEFAULT '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'";
     g_free(rkdb_create(TABLE_EVENT_MOTION_DETECTION, col_para));
     g_free(rkdb_insert(TABLE_EVENT_MOTION_DETECTION, "id", "0"));
+
+    col_para = "id INTEGER PRIMARY KEY AUTOINCREMENT," \
+               "iEnabled INT DEFAULT 0," \
+               "iTimeThreshold INT DEFAULT 3," \
+               "iProportion INT DEFAULT 0," \
+               "iSensitivityLevel INT DEFAULT 50," \
+               "iPositionX INT DEFAULT 0," \
+               "iPositionY INT DEFAULT 0," \
+               "iWidth INT DEFAULT 120," \
+               "iHeight INT DEFAULT 92";
+    g_free(rkdb_create(TABLE_EVENT_REGIONAL_INVASION, col_para));
+    g_free(rkdb_insert(TABLE_EVENT_REGIONAL_INVASION, "id", "0"));
 
     col_para = "id INTEGER PRIMARY KEY AUTOINCREMENT," \
                "iPromptVolume INT DEFAULT 50," \
