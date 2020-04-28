1c27200fa290c72827492f10071ed78b5e6778be
Jinkun Hong <jinkun.hong@rock-chips.com>
2020-04-28 18:21:45 +0800

    dbserver: Increase incoming parameters
    Signed-off-by: Jinkun Hong <jinkun.hong@rock-chips.com>
    Change-Id: I19641c131aa299c2a77ded48e7ff379038acce11

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <inttypes.h>
+#include <libgen.h>
 
 #include <glib.h>
 
@@ -23,17 +24,35 @@
 #include "system.h"
 #include "event.h"
 
-int main( int argc , char ** argv)
+int main(int argc , char ** argv)
 {
     GMainLoop *main_loop;
     DBusError dbus_err;
     DBusConnection *dbus_conn;
+    char *db_file = "/userdata/sysconfig.db";
+    char *db_path;
+
+    if (argc == 2)
+        db_file = argv[1];
+
+    db_path = g_strdup(db_file);
+    db_path = dirname(db_path);
+
+    if (access(db_path, 0)) {
+        printf("dbserver: %s folder does not exist\n", db_path);
+        return 0;
+    }
+
+    if (db_path)
+        g_free(db_path);
 
     dbus_error_init(&dbus_err);
     dbus_conn = g_dbus_setup_bus(DBUS_BUS_SYSTEM, DB_SERVER, &dbus_err);
 
     main_loop = g_main_loop_new(NULL, FALSE);
-    rkdb_init("/userdata/sysconfig.db");
+
+    rkdb_init(db_file);
+
     network_init(dbus_conn);
     storage_init(dbus_conn);
     media_init(dbus_conn);
