343826792739c32ecdae1f7e9f4efb6567673c01
Fenrir Lin <fenrir.lin@rock-chips.com>
2020-04-29 20:27:39 +0800

    dbserver: event: fixup iAccessCardNumber initial value and type do not match
    Signed-off-by: Fenrir Lin <fenrir.lin@rock-chips.com>
    Change-Id: Iae4df434f139d6ded148f164ad201c25b2c520e9

--- event.c
+++ event.c
@@ -119,7 +119,7 @@ void event_init(DBusConnection *dbus_conn)
                "sTelephoneNumber TEXT DEFAULT ''," \
                "sHometown TEXT DEFAULT ''," \
                "sAddress TEXT DEFAULT ''," \
-               "iAccessCardNumber TEXT DEFAULT 0," \
+               "iAccessCardNumber INT DEFAULT 0," \
                "iLoadCompleted INT DEFAULT 0," \
                "sNote TEXT DEFAULT ''";
     g_free(rkdb_create(TABLE_FACE_LIST, col_para));
